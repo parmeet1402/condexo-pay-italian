53b62486b90001b49c4279cd130355fc16262682
Parmeet Singh <parmeet.singh@classicinformatics.com>
2019-10-10 11:41:38 +0000

    App.js edited online with Bitbucket

--- src/App.js
+++ src/App.js
@@ -37,7 +37,6 @@ function App() {
       <ThemeProvider theme={theme}>
         <Fragment>
           <div className="App">
-            <h1>Sadf</h1>
             <Router history={history}>
               <Switch>
                 <Route exact path="/login" component={Login} />
